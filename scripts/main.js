68dfda79bbb3bf921c8f88ba005096a8895a56a2
Yongwei Che <59100012+YongweiChe@users.noreply.github.com>
2021-01-24 01:13:46 -0500

    First Website

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/yosemite.png') {
-      myImage.setAttribute('src','images/mocha.png');
+    if(mySrc === 'images/profile1.jpg') {
+      myImage.setAttribute('src','images/profile2.jpg');
     } else {
-      myImage.setAttribute('src','images/yosemite.png');
+      myImage.setAttribute('src','images/profile1.jpg');
     }
 }
